b7be617e27f764d8871aafec48defe5bc41c0608
Manilha7 <a58604@ualg.pt>
2019-10-30 11:39:14 +0000

    r

--- register_action.php
+++ register_action.php
@@ -18,30 +18,30 @@ include 'db.php';
     $password_corfirmed= $_POST['password-confirmed'];
     $password_final= substr(md5($_POST['password']),0,32);
            
-    $queryemail=" SELECT * from users where email= '$email'";
+    $queryemail=" SELECT * from users where email= $email";
     if(!($result= @ mysql_select_db($queryemail,$db))){
         showerror();
     }
     $nrows  = mysql_num_rows($result);
     if ($nrows>0) {
-        $ErrorType=1;
+        $Error=1;
         header("Location: register.php?Error=$Error&username=$username");
     }
 
     elseif ($password!=$password_corfirmed) {
-      $ErrorType=4; 
+      $Error=4; 
       header("Location: register.php?Error=$Error&email=$email&username=$username");
     }
 
     elseif (empty($password) || empty($password_corfirmed)) {
-        $ErrorType=3;
+        $Error=3;
       header("Location: register.php?Error=$Error&Email=$Email&Username=$Username"); 
     }
 
     elseif (empty($password) && empty($password_corfirmed) && empty($username) && empty(email)) {
-       $ErrorType=0;
+       $Error=0;
        header("Location: register.php?Error=$Error");
-    }
+    
     else{
         $sql_insert = "INSERT INTO users(name, email, created_at, updated_at, password_digest) VALUES('$username','$email',NOW(),NOW(),'$password_final')"; 
         header("Location: register_sucess.html");
